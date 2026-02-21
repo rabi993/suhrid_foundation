a6d1802d69fb9e06812e1288a018daffb7f2726d
rabi993 <rabi.21.pust@gmail.com>
2026-02-21 10:28:28 +0600

    env file added

--- suhrid_foundation/settings.py
+++ suhrid_foundation/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c5oy8a5vn9ov8be90v-0%+6$gcfug4+(7_b(1ar-s9zwm+jo_a"
+# SECRET_KEY = "django-insecure-c5oy8a5vn9ov8be90v-0%+6$gcfug4+(7_b(1ar-s9zwm+jo_a"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
